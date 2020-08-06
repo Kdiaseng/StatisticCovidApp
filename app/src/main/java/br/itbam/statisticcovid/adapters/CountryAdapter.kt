e97671bbd6595dd378e850539dba8a6efda990da
kaleb.silva <kaleb.silva@itbam.org.br>
2020-08-06 09:39:46 -0400

    applied databing in recyclerview

--- app/src/main/java/br/itbam/statisticcovid/adapters/CountryAdapter.kt
+++ app/src/main/java/br/itbam/statisticcovid/adapters/CountryAdapter.kt
@@ -1,28 +1,25 @@
 package br.itbam.statisticcovid.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import br.itbam.statisticcovid.R
 import br.itbam.statisticcovid.data.Country
 import br.itbam.statisticcovid.databinding.ItemCountryBinding
-import kotlinx.android.synthetic.main.item_country.view.*
-
 
 class CountryAdapter(private val countries: ArrayList<Country>) :
     RecyclerView.Adapter<CountryAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View ) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(private val itemCountryBinding: ItemCountryBinding) :
+        RecyclerView.ViewHolder(itemCountryBinding.root) {
         fun bindView(country: Country) {
-           itemView.tvNameCountry.text = country.country
-           itemView.tvCodeCountry.text = country.countryCode
+            itemCountryBinding.item = country
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_country,parent, false)
-        return ViewHolder(view)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemCountryBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount() = countries.size
--- app/src/main/java/br/itbam/statisticcovid/views/fragments/CountriesFragment.kt
+++ app/src/main/java/br/itbam/statisticcovid/views/fragments/CountriesFragment.kt
@@ -1,16 +1,16 @@
 package br.itbam.statisticcovid.views.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import br.itbam.statisticcovid.R
 import br.itbam.statisticcovid.adapters.CountryAdapter
 import br.itbam.statisticcovid.viewmodels.GeneralViewModel
+import br.itbam.statisticcovid.databinding.FragmentCountriesBinding
 import kotlinx.android.synthetic.main.fragment_countries.*
 
 class CountriesFragment : Fragment() {
@@ -20,9 +20,14 @@ class CountriesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        generalViewModel = ViewModelProvider(requireActivity()).get(GeneralViewModel::class.java)
 
-        return inflater.inflate(R.layout.fragment_countries, container, false)
+        val binding = FragmentCountriesBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        generalViewModel = ViewModelProvider(requireActivity()).get(GeneralViewModel::class.java)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,7 +40,8 @@ class CountriesFragment : Fragment() {
     private fun loadRecyclerView() {
         generalViewModel.countriesLiveData.observe(viewLifecycleOwner, Observer {
             rvCountries.adapter = CountryAdapter(it)
-            rvCountries.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+            rvCountries.layoutManager =
+                StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
         })
     }
 
