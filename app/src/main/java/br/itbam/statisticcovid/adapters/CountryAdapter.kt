f9e0c05d05060cdb5ea7e166c03cfede40094787
kaleb.silva <kaleb.silva@itbam.org.br>
2020-08-07 09:51:56 -0400

    implemented search countries

--- app/src/main/java/br/itbam/statisticcovid/adapters/CountryAdapter.kt
+++ app/src/main/java/br/itbam/statisticcovid/adapters/CountryAdapter.kt
@@ -2,16 +2,25 @@ package br.itbam.statisticcovid.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import br.itbam.statisticcovid.data.Country
 import br.itbam.statisticcovid.databinding.ItemCountryBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class CountryAdapter(
     private val countries: ArrayList<Country>,
     val onItemClick: (country: Country) -> Unit,
     val onClickStar: (country: Country) -> Unit
 ) :
-    RecyclerView.Adapter<CountryAdapter.ViewHolder>() {
+    RecyclerView.Adapter<CountryAdapter.ViewHolder>(), Filterable {
+    private var countryFilterList = ArrayList<Country>()
+
+    init {
+        countryFilterList = countries
+    }
 
     private var index: Int = -1
 
@@ -24,10 +33,10 @@ class CountryAdapter(
         return ViewHolder(binding)
     }
 
-    override fun getItemCount() = countries.size
+    override fun getItemCount() = countryFilterList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val country = countries[position]
+        val country = countryFilterList[position]
         holder.itemCountryBinding.item = country
 
         holder.itemCountryBinding.setShowDetails {
@@ -41,5 +50,36 @@ class CountryAdapter(
         }
         country.isSelected = index == position
 
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+
+                if (charSearch.isEmpty()) {
+                    countryFilterList = countries
+                } else {
+                    val resultList = ArrayList<Country>()
+                    for (row in countries) {
+                        if (row.country.toLowerCase(Locale.ROOT)
+                                .contains(charSearch.toLowerCase(Locale.ROOT))
+                        ) {
+                            resultList.add(row)
+                        }
+                        countryFilterList = resultList
+                    }
+                }
+                val filterResults = FilterResults()
+                filterResults.values = countryFilterList
+                return filterResults
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                countryFilterList = results?.values as ArrayList<Country>
+                notifyDataSetChanged()
+            }
+
+        }
     }
 }
--- app/src/main/java/br/itbam/statisticcovid/views/fragments/CountriesFragment.kt
+++ app/src/main/java/br/itbam/statisticcovid/views/fragments/CountriesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,11 +16,11 @@ import br.itbam.statisticcovid.data.Country
 import br.itbam.statisticcovid.databinding.FragmentCountriesBinding
 import br.itbam.statisticcovid.utils.SharedPreferenceUtils
 import br.itbam.statisticcovid.viewmodels.GeneralViewModel
-import br.itbam.statisticcovid.views.MainActivity
 import kotlinx.android.synthetic.main.fragment_countries.*
 
 class CountriesFragment : Fragment() {
     private lateinit var generalViewModel: GeneralViewModel
+    private  var adapter: CountryAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,21 +38,35 @@ class CountriesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        country_search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter?.filter?.filter(newText)
+                return false
+            }
+
+        })
+
         loadRecyclerView()
 
     }
 
     private fun loadRecyclerView() {
         generalViewModel.countriesLiveData.observe(viewLifecycleOwner, Observer {
-            rvCountries.adapter = CountryAdapter(it, { country ->
-                val action =
-                    CountriesFragmentDirections.actionCountriesFragmentToDetailsFragment(country)
-                rvCountries.findNavController().navigate(action)
-            }, {
-                saveCountryInSharedPreference(it)
-            })
+           adapter = CountryAdapter(it, { country ->
+               val action =
+                   CountriesFragmentDirections.actionCountriesFragmentToDetailsFragment(country)
+               rvCountries.findNavController().navigate(action)
+           }, {
+               saveCountryInSharedPreference(it)
+           })
             rvCountries.layoutManager =
                 StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+            rvCountries.adapter = adapter
         })
     }
 
